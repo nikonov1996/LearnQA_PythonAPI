8ec56eda54ac0a818debcde6bc00ad16fc675c81
nva <nva.1996@yandex.ru>
2021-12-07 00:35:58 +0700

    Added allure methods in test test_user_auth.py

--- tests/test_user_auth.py
+++ tests/test_user_auth.py
@@ -2,7 +2,9 @@ import pytest
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
 from lib.my_requests import MyRequests
+import allure
 
+@allure.epic("Authorization cases")
 class TestUserAuth(BaseCase):
     exclude_params = [
         ("no_cookie"),
@@ -24,7 +26,7 @@ class TestUserAuth(BaseCase):
         self.token = self.get_header(response1,"x-csrf-token")
         self.user_id_from_auth_method = self.get_json_value(response1,"user_id")
 
-
+    @allure.description("This test successfully authorize user by email and password")
     def test_auth_user(self):
         response2 = MyRequests.get(
             "/user/auth",
@@ -39,7 +41,7 @@ class TestUserAuth(BaseCase):
             "User id from auth method is not equal to user id from check method"
         )
 
-
+    @allure.description("This test checks authorization status w/o sending auth cookie or token")
     @pytest.mark.parametrize('condition',exclude_params)
     def test_negative_auth_check(self, condition):
         if condition == "no_cookie":
