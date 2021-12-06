190b05d0303cc429e3a77a3544ccd94dc4edcc7f
nva <nva.1996@yandex.ru>
2021-12-06 23:32:16 +0700

    Fixed other tests: test_user_edit.py test_user_get.py test_user_register.py

--- tests/test_user_edit.py
+++ tests/test_user_edit.py
@@ -1,14 +1,14 @@
-import requests
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
+from lib.my_requests import MyRequests
 
 class TestUserEdit(BaseCase):
 
     def test_edit_just_created_user(self):
         # REGISTER
         register_data = self.prepare_registration_data()
-        response1 = requests.post(
-            "https://playground.learnqa.ru/api/user/",
+        response1 = MyRequests.post(
+            "/user/",
             data=register_data
         )
 
@@ -27,8 +27,8 @@ class TestUserEdit(BaseCase):
             "password" : password
         }
 
-        response2 = requests.post(
-            "https://playground.learnqa.ru/api/user/login",
+        response2 = MyRequests.post(
+            "/user/login",
             data=login_data
         )
 
@@ -38,8 +38,8 @@ class TestUserEdit(BaseCase):
         # EDIT
         new_name = "Changed Name"
 
-        response3 = requests.put(
-            f"https://playground.learnqa.ru/api/user/{user_id}",
+        response3 = MyRequests.put(
+            f"/user/{user_id}",
             headers={"x-csrf-token":token},
             cookies={"auth_sid":auth_sid},
             data={"firstName": new_name}
@@ -48,8 +48,8 @@ class TestUserEdit(BaseCase):
         Assertions.assert_code_status(response3,200)
 
         # GET
-        response4 = requests.get(
-            f"https://playground.learnqa.ru/api/user/{user_id}",
+        response4 = MyRequests.get(
+            f"/user/{user_id}",
             headers={"x-csrf-token": token},
             cookies={"auth_sid": auth_sid},
         )
--- tests/test_user_register.py
+++ tests/test_user_register.py
@@ -1,16 +1,13 @@
 import requests
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
-from _datetime import datetime
+from lib.my_requests import MyRequests
 
 class TestUserRegister(BaseCase):
 
     def test_create_user_successfully(self):
         data = self.prepare_registration_data()
-        response = requests.post(
-            "https://playground.learnqa.ru/api/user",
-            data=data
-        )
+        response = MyRequests.post("/user", data=data)
 
         Assertions.assert_code_status(response, 200)
         Assertions.assert_json_has_key(response,"id")
@@ -19,10 +16,7 @@ class TestUserRegister(BaseCase):
         email = "vinkotov@example.com"
         data = self.prepare_registration_data(email)
 
-        response = requests.post(
-            "https://playground.learnqa.ru/api/user",
-            data=data
-        )
+        response = MyRequests.post( "/user", data=data )
 
         Assertions.assert_code_status(response,400)
         assert response.content.decode("utf-8") == f"Users with email '{email}' already exists",\
