a25f8aadeda2d2c927b397298ca2f3e05af3a5f8
nva <nva.1996@yandex.ru>
2021-12-10 00:03:31 +0700

    Added hw9.py

---
+++ home_work/hw9.py
@@ -0,0 +1,63 @@
+import requests
+
+passwords = [
+    '123456',
+    '123456789',
+    'qwerty',
+    'password',
+    '1234567',
+    '12345678',
+    '12345',
+    'iloveyou',
+    '111111',
+    '123123',
+    'abc123',
+    'qwerty123',
+    '1q2w3e4r',
+    'admin',
+    'qwertyuiop',
+    '654321',
+    '555555',
+    'lovely',
+    '7777777',
+    'welcome',
+    '888888',
+    'princess',
+    'dragon',
+    'password1',
+    '123qwe']
+url = "https://playground.learnqa.ru/api/"
+correct_login = "super_admin"
+correct_password = ''
+for password in passwords:
+
+    response = requests.post(
+        f"{url}get_secret_password_homework",
+        data={
+            'login' : correct_login,
+            'password' : password
+            }
+    )
+
+    if response.status_code == 200:
+        #auth_cookie = response.json()['auth_cookie']
+        auth_cookie = "mock_cookie"
+    else:
+        auth_cookie = "mock_cookie"
+
+
+    response1 = requests.post(
+        f"{url}check_auth_cookie",
+        data={
+            'auth_cookie' : auth_cookie
+        }
+    )
+
+    if response1.text == 'You are authorized':
+        print(f"Login: {correct_login}. Password: {password} is correct!")
+        correct_password = password
+        break
+    else:
+        print(f"Login: {correct_login}. Password: {password} is NOT correct!")
+
+    print("--------------")
