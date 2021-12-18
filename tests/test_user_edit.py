2bcff53c060430a02711b4ddce992e5bd8a542fc
nva <nva.1996@yandex.ru>
2021-12-18 23:49:50 +0700

    Added new tests to home work #17 in test_user_get.py

--- tests/test_user_edit.py
+++ tests/test_user_edit.py
@@ -1,3 +1,5 @@
+import pytest
+
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
 from lib.my_requests import MyRequests
@@ -169,4 +171,83 @@ class TestUserEdit(BaseCase):
             "firstName",
             user1_first_name,
             "Wrong name of the user after incorrect edit. 'firstName' must not be edited"
+        )
+
+    incorrect_params = {"email","firstName"}
+    @pytest.mark.parametrize("incorrect_parameter",incorrect_params)
+    def test_edit_just_created_user_with_incorrect_data(self,incorrect_parameter):
+        # REGISTER
+        register_data = self.prepare_registration_data()
+        response1 = MyRequests.post(
+            "/user/",
+            data=register_data
+        )
+
+        Assertions.assert_code_status(response1,200)
+        Assertions.assert_json_has_key(response1,"id")
+
+        email = register_data["email"]
+        first_name = register_data["firstName"]
+        password = register_data["password"]
+        user_id = self.get_json_value(response1,"id")
+
+
+        # LOGIN
+        login_data = {
+            "email" : email,
+            "password" : password
+        }
+
+        response2 = MyRequests.post(
+            "/user/login",
+            data=login_data
+        )
+
+        auth_sid = self.get_cookie(response2,"auth_sid")
+        token = self.get_header(response2,"x-csrf-token")
+
+        # EDIT
+
+        if incorrect_parameter == "email":
+            data_for_edit = {"email" : "email_without_symbol.mail.ru"}
+        else:
+            data_for_edit = {"firstName": "q"}
+
+        response3 = MyRequests.put(
+            f"/user/{user_id}",
+            headers={"x-csrf-token":token},
+            cookies={"auth_sid":auth_sid},
+            data=data_for_edit
+        )
+
+        Assertions.assert_code_status(response3,400)
+        if incorrect_parameter == "email":
+            Assertions.assert_response_text(response3,"Invalid email format")
+        elif incorrect_parameter == "firstName":
+            Assertions.assert_json_value_by_name(
+                response3,
+                "error",
+                "Too short value for field firstName",
+                "Wrong error message when short length of response param 'firstName'")
+
+        # GET . CHECK THAT USER DATA WAS NOT EDITED WITH INCORRECT PARAMETERS
+        response4 = MyRequests.get(
+            f"/user/{user_id}",
+            headers={"x-csrf-token": token},
+            cookies={"auth_sid": auth_sid},
+        )
+
+        Assertions.assert_json_value_by_name(
+            response4,
+            "firstName",
+            first_name,
+            f"'firstName' param must not be changed after incorrect edit request. "
+            f"Expect value: {first_name} "
+        )
+        Assertions.assert_json_value_by_name(
+            response4,
+            "email",
+            email,
+            f"'email' param must not be changed after incorrect edit request. "
+            f"Expect value: {email} "
         )
